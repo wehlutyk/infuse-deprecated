896070a5566eae87cb88dce264c1233294ee108d
Sébastien Lerique <sl@mehho.net>
2016-06-01 15:39:53 +0200

    refactor/lib: move all main stuff into lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,21 @@
 #![plugin(diesel_codegen, dotenv_macros)]
 
 extern crate iron;
-extern crate router;
-#[macro_use]
-extern crate diesel;
+#[macro_use] extern crate router;
+#[macro_use] extern crate diesel;
+extern crate logger;
 extern crate dotenv;
 
 pub mod schema;
 pub mod models;
 pub mod views;
 
+use iron::prelude::*;
+use iron::error::HttpResult;
+use iron::Listening;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
+use logger::Logger;
 use dotenv::dotenv;
 use std::env;
 
@@ -21,4 +25,26 @@ pub fn establish_connection() -> PgConnection {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub struct Infuse {
+    pub server: Iron<Chain>,
+}
+
+impl Infuse {
+    pub fn new() -> Infuse {
+        let router = router!(get "/documents" => views::documents_handler,
+                             get "/documents/:id" => views::document_handler);
+        let (logger_before, logger_after) = Logger::new(None);
+
+        let mut chain = Chain::new(router);
+        chain.link_before(logger_before);
+        chain.link_after(logger_after);
+
+        Infuse { server: Iron::new(chain) }
+    }
+
+    pub fn serve(self) -> HttpResult<Listening> {
+        self.server.http("localhost:3000")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 extern crate infuse;
-extern crate iron;
-#[macro_use]
-extern crate router;
-extern crate logger;
 
-use iron::prelude::*;
-use logger::Logger;
-use infuse::views;
-
-// 3 endpoints:
-// - /process: send a pdf to process, get link to job
-// - /jobs/:id: get active/completed jobs
-// - /documents/:id: get processed documents
+use infuse::Infuse;
 
 fn main() {
-    let router = router!(get "/documents" => views::documents_handler,
-                         get "/documents/:id" => views::document_handler);
-    let (logger_before, logger_after) = Logger::new(None);
-
-    let mut chain = Chain::new(router);
-    chain.link_before(logger_before);
-    chain.link_after(logger_after);
-
-    Iron::new(chain).http("localhost:3000").unwrap();
+    Infuse::new().serve().unwrap();
 }
