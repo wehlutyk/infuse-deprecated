3ca42575a986ca7a238949cad39bc6755e9e8421
Sébastien Lerique <sl@mehho.net>
2016-06-01 15:19:15 +0200

    feat/models: add insertable document

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,14 @@
+use schema::documents;
+
 #[derive(Queryable)]
 pub struct Document {
     pub id: i32,
     pub hash: String,
     pub tei: String,
+}
+
+#[insertable_into(documents)]
+pub struct NewDocument<'a> {
+    pub hash: &'a str,
+    pub tei: &'a str,
 }
